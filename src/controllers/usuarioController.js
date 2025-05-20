a151b6fdb735d9e1d34ff9031b0f762ac31a51c1
LPAmorim <lucas.santos058@sptech.school>
2025-05-20 08:28:44 -0300

    add modificações de routers e atualizando as mesmas

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -73,14 +73,12 @@ function cadastrar(req, res) {
     // Faça as validações dos valores | da pra simplificar
     if (nome == undefined) {
         res.status(400).send("Seu nome está undefined!");
+    } else if (apelido == undefined) {
+        res.status(400).send("Seu apelido está undefined!");
     } else if (email == undefined) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
-    } else if (fkEmpresa == undefined) {
-        res.status(400).send("Sua empresa a vincular está undefined!");
-    } else if (cpf == undefined) {
-        res.status(400).send("Seu CPF está undefined!");
     } else {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
