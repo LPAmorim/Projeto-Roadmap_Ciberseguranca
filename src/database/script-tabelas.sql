cefd770550e6523abbb6d7b9fda7125a3eb4e4f2
LPAmorim <lucas.santos058@sptech.school>
2025-06-07 20:16:50 -0300

    raw add novas regras e tabelas para o quiz

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -8,8 +8,6 @@ CREATE TABLE users (
     apelido VARCHAR(12) UNIQUE,
     email VARCHAR(50) UNIQUE,
     senha VARCHAR(50) NOT NULL
-    -- insight_id INT,
-    -- FOREIGN KEY (insight_id) REFERENCES insight(id)
 );
 
 -- Tabela base de filmes/séries
@@ -21,6 +19,9 @@ CREATE TABLE filmes_series (
     tempo_de_duracao INT
 );
 
+insert into users (nome, apelido, email, senha)values
+	('Lucas Amorim', 'Amorim', 'amorim@gmail.com', 'Home3549#');
+
 CREATE TABLE insight (
     id INT PRIMARY KEY AUTO_INCREMENT,
     fkuser INT,
@@ -30,6 +31,92 @@ CREATE TABLE insight (
     FOREIGN KEY (fkfilmes_series) REFERENCES filmes_series(id),
     UNIQUE (fkuser, fkfilmes_series)
 );
+
+CREATE Table quiz_perguntas (
+    id int PRIMARY KEY AUTO_INCREMENT,
+    pergunta TEXT
+);
+
+CREATE table alternativas (
+    id int PRIMARY key AUTO_INCREMENT,
+    fkquiz_perguntas int,
+    texto varchar(200),
+    valor int,
+    UNIQUE(fkquiz_perguntas, texto),
+    UNIQUE(fkquiz_perguntas, valor),
+    foreign key (fkquiz_perguntas) references quiz_perguntas(id)
+);
+
+CREATE table feedback (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    analise VARCHAR(255),
+    classe VARCHAR(50),
+    pontuacao_min int,
+    pontuacao_max int,
+    check (classe in ('Script Kids', 'Problem Solver', 'Cyber Architect'))
+);
+
+CREATE table ficha_tecnica (
+    fkuser int,
+    fkquiz_perguntas int,
+    pontuacao int,
+    dataRealizada DATE,
+    fkfeedback int,
+    PRIMARY KEY(fkuser, fkquiz_perguntas),
+    FOREIGN KEY (fkuser) REFERENCES users(id),
+    FOREIGN KEY (fkquiz_perguntas) REFERENCES quiz_perguntas(id),
+    FOREIGN KEY (fkfeedback) REFERENCES feedback(id)
+);
+
+INSERT INTO quiz_perguntas (pergunta) VALUES
+  ('Você está acampando e percebe que esqueceu de levar comida. O que você faz?'),
+  
+  ('Você está no meio de um projeto em andamento, já finalizando, mas percebe que esqueceu de comentar as funções e deixar o código limpo. O que você faz?'),
+  
+  ('Se você precisasse aprender a usar uma nova tecnologia ou ferramenta, como você faria?'),
+  
+  ('Em um ambiente de trabalho. Você está construindo várias funções de código parecidas e percebe que está ficando repetitivo, deixando o código pesado. O que você faz?'),
+  
+  ('Você está em um grupo onde começam a falar mal de uma pessoa conhecida de todos, inclusive sua. Qual atitude você toma?');
+
+    
+INSERT INTO alternativas (fkquiz_perguntas, texto, valor)VALUES
+    ('1', 'Volto para casa imediatamente.', 1),
+    ('1', 'Reclamo da situação e tento aguentar um dia sem comer.', 2),    
+    ('1', 'Analiso os arredores e tento encontrar algum alimento.', 3), 
+    ('1', 'Peço ajuda para alguém próximo ou vizinho de acampamento.', 4),
+    ('1', 'Volto até um lugar onde passei e vi que tinha uma loja para comprar comida
+    e continuo acampando.', 5),
+    
+    ('2', 'Ignoro, afinal está funcionando.', 1),
+    ('2', 'utilizo uma IA para comentar e otimizar o meu código', 2),
+    ('2', 'Anoto para corrigir depois e entrego o projeto.', 3),
+	('2', 'Comento só o que achar mais importante e envio.', 4),
+    ('2', 'Faço uma revisão completa, aplico boas práticas e otimizo antes de finalizar.', 5),
+	
+    ('3', 'Procuro um vídeo rápido no YouTube e copio os passos.', 1),
+    ('3', 'Pergunto para alguém me ensinar direto.', 2),
+    ('3', 'Utilizo uma IA para gerar uma documentação resumida e aplico na prática.', 3),
+    ('3', 'Faço um curso rápido e coloco em prática com projetos.', 4), 
+    ('3', 'Estudo a documentação oficial e crio anotações para revisar e ensinar.', 5),
+    
+    ('4', 'Deixo assim mesmo, ninguém falou que tinha que deixar o código leve.', 1),
+    ('4', 'Deixo o serviço para outra pessoa fazer, meu código está funcional e isso que importa.', 2),
+    ('4', 'Tento organizar um pouco para reduzir a bagunça.', 3),
+    ('4', 'Crio funções reutilizáveis para limpar o código.', 4), 
+    ('4', 'Refatoro toda a lógica e aplico padrões de projeto.', 5),
+    
+      (5, 'Fico quieto e acompanho a conversa.', 1),
+  (5, 'Dou risada, mas não digo nada.', 2),
+  (5, 'Tento mudar de assunto.', 3),
+  (5, 'Levanto e saio, porque não acho certo participar disso.', 4), 
+  (5, 'Questiono o grupo e tento conscientizar sobre respeito e ética.', 5);
+    
+INSERT INTO feedback (analise, classe, pontuacao_min, pontuacao_max)VALUES
+    ('Você está começando a explorar o mundo da tecnologia. Ainda age por impulso, mas tem potencial para crescer com prática e paciência.', 'Script Kids', 5, 10),
+    ('Você demonstra capacidade de pensar logicamente e resolver problemas com eficiência. Tem uma boa base e tende a evoluir rapidamente.', 'Problem Solver', 11, 17),
+    ('Você já pensa como um arquiteto de sistemas. É estratégico, ético e altamente persistente. Um perfil de liderança técnica.', 'Cyber Architect', 18, 25);
+
 
 INSERT INTO filmes_series (titulo, ano_De_Lancamento, categoria, tempo_de_duracao) VALUES
     ('Hacker: Todo Crime tem um Início', 2016, 'Suspense', 95),
@@ -46,7 +133,7 @@ SELECT ROUND(AVG(nota), 1) FROM insight WHERE fkfilmes_series = 2;
 SELECT * from users;
 SELECT*from filmes_series;
 SELECT*from insight;
-
+select * from quiz_perguntas;
 
 drop TABLE insight;
 DROP DATABASE roadmap_cyber;
@@ -56,76 +143,8 @@ ALTER TABLE filmes_series AUTO_INCREMENT = 1
 
 
 
- SELECT id, nome, apelido, email, senha FROM users WHERE (email = '' OR 1=1 order by 1) -- "' or apelido = '' OR 1=1 order by 1) -- "') AND senha = 'ASD';\n` 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- Tabela de insights ()
--- CREATE TABLE insight (
---     id INT PRIMARY KEY AUTO_INCREMENT,
---     fkuser INT,
---     fkfilmes_series INT,
---     tipo ENUM('pessoal', 'geral'),
---     nota DECIMAL(3,1), -- exemplo: 10.0
---     FOREIGN KEY (fkuser) REFERENCES users(id),
---     FOREIGN KEY (fkfilmes_series) REFERENCES filmes_series(id)
--- );
-
--- Questionários
--- CREATE TABLE questionario (
---     id INT PRIMARY KEY AUTO_INCREMENT,
---     titulo VARCHAR(100),
---     descricao TEXT
--- );
-
--- -- Tentativas de usuários em questionários
--- CREATE TABLE tentativa (
---     id INT PRIMARY KEY AUTO_INCREMENT,
---     usuario_id INT,
---     questionario_id INT,
---     pontuacao INT,
---     data_tentativa DATETIME,
---     FOREIGN KEY (usuario_id) REFERENCES users(id),
---     FOREIGN KEY (questionario_id) REFERENCES questionario(id)
--- );
-
--- Perguntas dos questionários
--- CREATE TABLE perguntas (
---     id INT PRIMARY KEY AUTO_INCREMENT,
---     questionario_id INT,
---     texto TEXT,
---     pontuacao_correta INT,
---     FOREIGN KEY (questionario_id) REFERENCES questionario(id)
--- );
-
--- Respostas que o usuário deu
--- CREATE TABLE respostasUser (
---     id INT PRIMARY KEY AUTO_INCREMENT,
---     tentativa_id INT,
---     pergunta_id INT,
---     resposta_usuario TEXT,
---     correta BOOLEAN,
---     FOREIGN KEY (tentativa_id) REFERENCES tentativa(id),
---     FOREIGN KEY (pergunta_id) REFERENCES perguntas(id)
--- );
+select id, nome, apelido, email, senha FROM users WHERE (email = '' OR 1=1 order by 1);
+ -- "' or apelido = '' OR 1=1 order by 1) -- "') AND senha = 'ASD';\n` 
+ 
+SELECT DATE_FORMAT(sua_coluna_de_data, '%d %y %Y') AS data_formatada
+FROM sua_tabela;
