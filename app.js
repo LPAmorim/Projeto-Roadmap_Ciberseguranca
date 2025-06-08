7d026d9e83db8016cff02a319215c70cc26249f1
LPAmorim <lucas.santos058@sptech.school>
2025-06-07 21:01:46 -0300

    raw arrumando parametros para Quizz

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var app = express();
 var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
 var insightRouter = require("./src/routes/insight");
-var quizRouter = require("./src/routes/quiz");
+var quizzRouter = require("./src/routes/quizz");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -29,7 +29,7 @@ app.use(cors());
 app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
 app.use("/insight", insightRouter);
-app.use("/quiz", quizRouter);
+app.use("/quizz", quizzRouter);
 
 app.listen(PORTA_APP, function () {
     console.log(`
