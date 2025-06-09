cb336a655486824e77329c7a350b1e682dcb2359
LPAmorim <lucas.santos058@sptech.school>
2025-06-09 14:21:49 -0300

    feat nova rota para lista Resultado do Questionario

--- src/routes/quizz.js
+++ src/routes/quizz.js
@@ -3,16 +3,16 @@ var router = express.Router();
 
 var quizzController = require("../controllers/quizzController");
 
-router.post("/inserir", function (req, res) {
-    quizzController.inserirDadosQuizz(req, res);
-});
-
-router.get("/listar", function (req, res) {
-    quizzController.listarResultadoQuizz(req, res);
+router.post("/inserindoResultadoQuiz", function (req, res) {
+    quizzController.inserindoResultadoQuiz(req, res);
 });
 
 router.get("/listarQuestoes", function (req, res) {
     quizzController.listarQuestoes(req, res);
+});
+
+router.get("/listar", function (req, res) {
+    quizzController.listarResultadoQuizz(req, res);
 });
 
 module.exports = router;
